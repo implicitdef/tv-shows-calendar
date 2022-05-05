a01f63d8bdbe2feb5474f7485c5570798fd131f7
implicitdef <implicitdef@users.noreply.github.com>
2022-05-05 22:59:08 +0200

    update default showws

--- src/server/utils/conf.ts
+++ src/server/utils/conf.ts
@@ -18,6 +18,10 @@ export const db = process.env.DATABASE_URL
     }
 export const defaultShowsIds = [
   // If you put too much of those, the first request starts to be a bit too long
+
+  // To find ids of shows, look them up on the search field in the app
+  // add them, and look in the network tab at the POST request
+
   '1399', // Game of Thrones
   // '1402', // Walking dead
   // '63247', // Westworld
@@ -27,20 +31,24 @@ export const defaultShowsIds = [
   // '1424', // Orange is the new black
   '1412', // Arrow
   '2190', // South park
-  '61733', // Empire,
+  // '61733', // Empire,
   '60625', // Rick and Morty
   // '38472', // jessica jones
   '61889', // daredevil
-  '67136', // this is us
+  // '67136', // this is us
   '42009', // black mirror
   '65494', // The crown
-  '67744', // Mindhunter
+  // '67744', // Mindhunter
   // '65708', // Taboo
   // '79242', // The chilling adventures of sabrina
   '60622', // Fargo
   // '78191', // You
-  '75006', // The umbrella academy
-  '84977', // Russian doll
+  // '75006', // The umbrella academy
+  // '84977', // Russian doll
+  '82856', // The mandalorian
+  '60574', // Peaky blinders
+  '93405', // Squid game
+  '82596', // Emily in Paris
 ]
 export const port = process.env.PORT || 3333
 export const pushDataApiKey = process.env.PUSH_DATA_API_KEY || 'pushDataApiKey'
