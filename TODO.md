2cc6480ff4c95fb383fd91ac5415592a3fabc0bd
implicitdef <implicitdef@users.noreply.github.com>
2022-05-24 18:34:08 +0200

    suite

---
+++ TODO.md
@@ -0,0 +1,5 @@
+- Brancher le front en mode non-authenticated
+- Puis faire l'auth avec iron session (essayer)
+- Puis faire une passe sur les endpoints, il y en a qui seront devenus inutiles
+- Puis repasser sur les controller pour cleaner un peu, mutualiser des petits trucs
+
--- src/pages/[year].tsx
+++ src/pages/[year].tsx
@@ -4,7 +4,10 @@ import { loadData } from "../server.core";
 type Data = { message: string };
 
 export const getServerSideProps = async () => {
-  const data: Data = await loadData();
+  const dataFromDb = await loadData();
+  // TODO get the year from the context and render some basic stuff
+  // TODO then use the data from db, get the correct series and seasons and so on, and display them roughly
+  // TODO then plus the frontend with a calendar and a bit of css
 
   return {
     props: {
