2c36b79c91155ecf755e7be743b3fe6fb939abbd
implicitdef <implicitdef@users.noreply.github.com>
2022-05-31 23:06:48 +0200

    suite

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 // TODO voir si ce reset est utile
+// TODO fix la hauteur du calendrier, et autres petits détails CSS
 import "../styles/globals.css";
 import "../styles/index.scss";
 // TODO voir si j'ai besoin de remettre le css de boostrap
@@ -7,6 +8,5 @@ import type { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
 
 export default MyApp;
--- src/server.auth.ts
+++ src/server.auth.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest } from "next";
 
 // TODO faire une vraie authentication plutôt qu'un paramètre de query
+// Utiliser "https://next-auth.js.org/" pour le mode passwordless
+// voir aussi https://nextjs.org/docs/authentication
 
 export function getConnectedUserId(req: NextApiRequest): string {
   const { userId } = req.query;
