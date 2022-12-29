5db3ae56bb09ab54802de5d8a66bed55c137e739
implicitdef <implicitdef@users.noreply.github.com>
2022-12-29 22:33:02 +0100

    read config vars differently, to be able to use new DB on droplet

--- src/server.conf.ts
+++ src/server.conf.ts
@@ -1,19 +1,19 @@
 import { PoolConfig } from 'pg'
 
-function reverse(s: string): string {
-    return s.split('').reverse().join('')
-}
-
 export const PUSH_DATA_API_KEY =
     process.env.PUSH_DATA_API_KEY || 'pushDataApiKey'
 
 export const JWT_SECRET = process.env.JWT_SECRET || 'jwtSecret'
 
-export const DB_POOL_CONFIG: PoolConfig = process.env.DATABASE_URL
+export const DB_POOL_CONFIG: PoolConfig = process.env.DB_HOST
     ? {
-          connectionString: `${process.env.DATABASE_URL}`,
+          //   connectionString: `${process.env.DATABASE_URL}`,
           // https://help.heroku.com/MDM23G46/why-am-i-getting-an-error-when-i-upgrade-to-pg-8
-          ssl: { rejectUnauthorized: false },
+          //   ssl: { rejectUnauthorized: false },
+          host: process.env.DB_HOST,
+          user: process.env.DB_USER,
+          password: process.env.DB_PWD,
+          database: process.env.DB_NAME,
       }
     : {
           host: '127.0.0.1',
@@ -21,3 +21,5 @@ export const DB_POOL_CONFIG: PoolConfig = process.env.DATABASE_URL
           password: '',
           database: 'tv_shows_calendar',
       }
+
+console.log('Using pool config with host : ', DB_POOL_CONFIG.host)
